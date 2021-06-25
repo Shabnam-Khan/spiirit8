e1cd846a7f6291a321c71a1bbae72279392eb107
Shabnam-Khan <shabnam0648@gmail.com>
2021-06-26 00:39:14 +0600

    boom

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(function(){
+    // banner slider
+    $('.banner-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows:false,
+        fade:true,
+        speed:2500,
+        dots:true,
+    });
+
+});
